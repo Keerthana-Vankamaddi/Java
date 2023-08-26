0390571df8e219d456fe76d741538563e3a72352
DELL <DELL@Keerthana>
2023-08-26 16:15:11 +0530

    second commit

---
+++ shilpa/src/shilpa/helloWorld.java
@@ -0,0 +1,17 @@
+package shilpa;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="keerthana";
+		char []c=s.toCharArray();
+		for(int i=0;i<c.length;i++) {
+		int ch=c[i];
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			
+System.out.println(c[i]);
+	}
+	}
+
+}
